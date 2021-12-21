db0cdabf99359602862f2c32c6203a16ab284d63
Krzysztof Neumann <krzysztof@interaktywni.net>
2021-12-21 09:30:18 +0100

    Add django_admin_panel app

---
+++ app/django_bootstrap_admin_panel/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangoBootstrapAdminPanelConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_bootstrap_admin_panel'
